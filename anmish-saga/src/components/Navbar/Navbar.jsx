5b1c2114a6c640a745e30ce244d1077dac4dbb46
kumarmanish562 <qmanishkumar562@gmail.com>
2025-03-05 16:28:01 +0530

    Added a responsive Navbar with dark mode support and mobile-friendly menu. Integrated a toggle feature for dark mode and included animated background effects in the Hero section for a better user experience.

--- anmish-saga/src/components/Navbar/Navbar.jsx
+++ anmish-saga/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,64 @@
-import React from 'react';
-import Logo from '../../assets/logo.png';
+import React, { useState } from "react";
+import Logo from "../../assets/logo.png";
+import { HiMenuAlt1, HiMenuAlt3 } from "react-icons/hi";
+import DarkMode from "./DarkMode";
+
+const NavLinks = [
+  { id: 1, name: "Home", link: "#" },
+  { id: 2, name: "Products", link: "#" },
+  { id: 3, name: "Pricing", link: "#" },
+  { id: 4, name: "Contact", link: "#" },
+];
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = () => setShowMenu(!showMenu);
+
   return (
-    <div>
-      <div className="container py-2 md:py-0">
-        <div className="flex items-center justify-between">
+    <div className="relative z-50 text-black dark:text-white duration-300">
+      <div className="container mx-auto py-2 md:py-0 px-4">
+        <div className="flex justify-between items-center">
           {/* Logo section */}
-          <div className="flex items-center gap-3">
-            <img src={Logo} alt="Logo" className="h-16" />
-            <p className="text-3xl">VR
-              <span className="font-bold">World</span>
+          <div className="flex items-center gap-2">
+            <img src={Logo} alt="Logo" className="h-14" />
+            <p className="text-2xl">
+              VR <span className="font-bold">World</span>
             </p>
           </div>
           {/* Desktop menu section */}
-          <div>{/* Add menu items here */}</div>
+          <nav className="hidden md:block">
+            <ul className="flex items-center gap-8">
+              {NavLinks.map(({ id, name, link }) => (
+                <li key={id} className="py-4">
+                  <a
+                    href={link}
+                    className="text-xl font-semibold hover:text-primary py-2 hover:border-b-2 hover:border-secondary transition-all duration-300"
+                  >
+                    {name}
+                  </a>
+                </li>
+              ))}
+              <DarkMode />
+            </ul>
+          </nav>
+
+          {/* Mobile View Sidebar */}
+          <div className="md:hidden block">
+            <div className="flex items-center gap-4">
+              <DarkMode />
+              {showMenu ? (
+                <HiMenuAlt3
+                  onClick={toggleMenu}
+                  className="cursor-pointer text-3xl"
+                />
+              ) : (
+                <HiMenuAlt1
+                  onClick={toggleMenu}
+                  className="cursor-pointer text-3xl"
+                />
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
